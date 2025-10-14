7eb33e7dcf1f48679ce3c7a764cee907e97cebd7
John Ivan Sinchi Nina <johnivansn@gmail.com>
2025-10-13 23:49:17 -0400

    feat(page): Integrar componente SortCard a la página de inicio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import SortCard from '@/components/sort/SortCard';
 import Image from 'next/image';
 
 export default function Home() {
@@ -22,8 +23,8 @@ export default function Home() {
           </li>
           <li className="tracking-[-.01em]">Save and see your changes instantly.</li>
         </ol>
-
         <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <SortCard />
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"

6a1356e774f31826b3b7e7c63e2f15f6598de2bb
Saraqch <quispechoquesara@gmail.com>
2025-10-20 19:37:20 -0400

    fix: url backend

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 // lib/api.ts
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://devmastersbackend.vercel.app';
 
 export interface ApiResponse<T> {
   success: boolean;

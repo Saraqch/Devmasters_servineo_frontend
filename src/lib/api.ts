e488db8fb83e23d4ca2fb7802de30163dc29359e
John Ivan Sinchi Nina <johnivansn@gmail.com>
2025-10-31 20:03:13 -0400

    feat: actualizar la URL base de la API para apuntar al entorno de producción

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 // lib/api.ts
-//export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://devmasters-servineo-backend-tdck.vercel.app';
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://devmasters-servineo-backend-tdck.vercel.app';
+//export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
 
 export interface ApiResponse<T> {
   success: boolean;

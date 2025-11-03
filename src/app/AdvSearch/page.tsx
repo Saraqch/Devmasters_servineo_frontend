c64af2950da2055e01c890543a734f147f17d4bd
Mel Espinoza <melespinozadelgado@gmail.com>
2025-11-02 21:25:33 -0400

    Creacion de la carpeta nueva AdvSearch con las subcarpetas components y hooks, para la nueva pagina de busqueda avanzada

---
+++ src/app/AdvSearch/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdvancedSearchPage = () => {
+  return (
+    <div>
+      <h1>Hola Mundo desde Búsqueda Avanzada</h1>
+    </div>
+  );
+};
+
+export default AdvancedSearchPage;

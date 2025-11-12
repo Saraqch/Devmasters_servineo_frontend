17bdccff973804467cb2ebb08bef427f40c9bc92
John Ivan Sinchi Nina <johnivansn@gmail.com>
2025-11-12 00:51:54 -0400

    style: corrige la capitalización de las rutas en aliases y actualiza dependencias en package.json y package-lock.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 module.exports = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Roboto', 'sans-serif'], //  Roboto como fuente por defecto
         roboto: ['Roboto', 'sans-serif'], //  También disponible como font-roboto
       },
-      colors : {
-        
-      }
+      colors: {},
     },
   },
   plugins: [],

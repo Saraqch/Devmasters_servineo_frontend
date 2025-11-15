f92de07e7aa0bbaa063a1ff489ae29a5d750bb61
johnivansn <johnivansn@gmail.com>
2025-11-15 05:16:36 -0400

    fix: corregir direcciones de imports

--- src/app/jobOfert/components_jo/Search/SearchButton.tsx
+++ src/app/jobOfert/components_jo/Search/SearchButton.tsx
@@ -1,5 +1,5 @@
 //SearchButton
-import { Button } from '@/components/ui/button';
+import { Button } from '@/Components/ui/button';
 import React from 'react';
 
 export function SearchButton(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
